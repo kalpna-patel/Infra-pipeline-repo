fcdd5fdcf9921f2ceee5beb9c47b2728c387615b
kalpna patel <kalpnapatel2911@gmail.com>
2024-08-08 19:29:50 +0530

    updated

--- Env/Dev/provider.tf
+++ Env/Dev/provider.tf
@@ -6,9 +6,9 @@ terraform {
     }
   }
     backend "azurerm" {
-    resource_group_name  = "kkp-rg"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "kpstr123"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "kpctr"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    resource_group_name  = "kp-rg"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "kpstr1234"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "cntrkp"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "kp.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
